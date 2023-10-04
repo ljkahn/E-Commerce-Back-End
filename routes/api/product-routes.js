9d2ae823b76f4b523260c225fbf42f501cc55f71
ljkahn <lia.j.kahn@gmail.com>
2023-10-04 15:37:48 -0600

    updated tag-routes.js and product-routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -9,10 +9,11 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Category and Tag data
   try {
     const productData = await Product.findAll({
-        include: [{ model: Category}], [{ model: Tag }]
+        include: [{ model: Category}, { model: Tag }]
     });
     res.status(200).json(productData);
   } catch(err) {
+    console.log(err);
     res.status(500).json(err);
   }
 
@@ -24,7 +25,7 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Category and Tag data
   try {
     const productData = await Product.findByPk(req.params.id, {
-      include: [{ model: Category, Tag }]
+      include: [{ model: Category}, {Tag }]
     });
 
     if (!productData) {
@@ -34,20 +35,13 @@ router.get('/:id', async (req, res) => {
 
     res.status(200).json(productData);
   } catch (err) {
+    console.log(err);
     res.status(500).json(err);
   }
 });
 
 // create new product
 router.post('/', async (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
@@ -118,8 +112,23 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+try {
+  const productData = await Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+
+  if (!productData) {
+    res.status(404).json({message: "No product found with that id!"})
+    return;
+  }
+  res.status(200).json(productData)
+} catch(err) {
+  res.status(500).json(err)
+}
 });
 
 module.exports = router;
